0eb2697b9496fca7bc5195e1806ffeb866811dbd
Mustafa Rahimi <mustrahimi098@gmail.com>
2022-08-30 17:04:56 +0430

    2 item deleted

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressEjs = require('express-ejs-layouts')
+var expressEjs = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,9 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('layout', './layout')
+app.set('layout', './layout');
 
-app.use(expressEjs)
+app.use(expressEjs);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
